889c2969cfcd21da95b7477890110cf7073c4648
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-01 23:30:54 +0530

    added exception for times where resources are not found.

--- src/main/java/com/anurag/worklog/exception/ResourceNotFoundException.java
+++ src/main/java/com/anurag/worklog/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.anurag.worklog.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
 }
