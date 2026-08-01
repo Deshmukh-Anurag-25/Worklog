0105e6419972ddc1041dccb906e12f658db21ad7
Anurag Deshmukh <236213990+Deshmukh-Anurag-25@users.noreply.github.com>
2026-08-01 23:30:17 +0530

    added exception for invalid credentials.

--- src/main/java/com/anurag/worklog/exception/InvalidCredentialsException.java
+++ src/main/java/com/anurag/worklog/exception/InvalidCredentialsException.java
@@ -1,4 +1,7 @@
 package com.anurag.worklog.exception;
 
-public class InvalidCredentialsException {
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
 }
